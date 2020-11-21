21a8cd6a70cd18147ce018031c9ebf9a1b710128
vjeuel <vjeuel@gmail.com>
2020-11-21 07:17:07 -0600

    changing React Base page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div className='App'>
+				<Navbar />
+				<Switch>
+					<Route path='/' exact component={withRouter(Home)} />
+					<Route path='/privacy' component={withRouter(Privacy)} />
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Pages.css';
+
+function Home() {
+	return;
+}
+
+export default About;
