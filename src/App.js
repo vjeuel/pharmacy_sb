cc5add1116f7f3334b09c84186acb20fa836518f
vjeuel <vjeuel@gmail.com>
2020-11-21 08:04:57 -0600

    Privacy Policy added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
+import Home from './pages/Home';
+import Privacy from './pages/Privacy';
 
 function App() {
 	return (
 		<Router>
 			<div className='App'>
-				<Navbar />
+				{/* <Navbar /> */}
 				<Switch>
 					<Route path='/' exact component={withRouter(Home)} />
 					<Route path='/privacy' component={withRouter(Privacy)} />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './Pages.css';
 
 function Home() {
-	return;
+	return (
+		<div>
+			<h1>Home</h1>
+		</div>
+	);
 }
 
-export default About;
+export default Home;
